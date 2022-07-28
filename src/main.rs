fd6d5fd3bc9dd34aa5ddfbb142cf79fd4f5e00d6
Mark Tomlin <dygear@O11-Mini.mynetworksettings.com>
2022-07-28 06:21:24 -0400

    UDP Setup and some helpers.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#![allow(non_snake_case)]
+
+use std::net::UdpSocket;
+use prctl::set_name;
+
+mod packet;
+
 fn main() {
-    println!("Hello, world!");
+    set_name("Timing and Scoring").expect("Couldn't set process title.");
+
+    let socket = UdpSocket::bind("0.0.0.0:20777").expect("Couldn't bind to address.");
+    println!("UDP Port Bound");
+
+    let mut buffer = [0; 4096];
+    loop
+    {
+        let (_numBytes, socketAddress) = socket.recv_from(&mut buffer).unwrap();
+        let packet: packet::Header = match packet::Header::unpack(&buffer)
+        {
+            Some(packet) => packet,
+            None => { continue; }
+        };
+    }
 }
